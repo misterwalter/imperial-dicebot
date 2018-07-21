7f0142f8dcc1d41ccc2611375c2d5a639d8c2f96
Walter <verburg.walt@alumni.uwlax.edu>
2018-07-21 18:59:33 +0000

    Reduced the dead time on the crashlooper script

--- dicebot.sh
+++ dicebot.sh
@@ -1,1 +1,1 @@
-while :; do python3 dicebot.py; echo "Dicebot went down!" >> dice.log; date >> dice.log; sleep 10; done
+while :; do python3 dicebot.py; echo "Dicebot went down!" >> dice.log; date >> dice.log; sleep 1; done
